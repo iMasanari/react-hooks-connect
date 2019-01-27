fdd01f7844ac58f633461438c4a9204d8acb4325
iMasanari <14dd023@gmail.com>
2019-01-27 20:30:48 +0900

    add pram of WrappedComponent to ConnectedComponent

--- src/react-hooks-connect.tsx
+++ src/react-hooks-connect.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
 
+interface ConnectedComponent<OwnProps, Props> extends React.FunctionComponent<OwnProps> {
+  WrappedComponent: React.ComponentType<Props>
+}
+
 const defaultMergeProps = (hooksProps: any, ownProps: any) => ({ ...ownProps, ...hooksProps })
 
 export default <OwnProps, HooksProps, Props = HooksProps>(
   mapHooks: (ownProps: OwnProps) => HooksProps,
   mergeProps = defaultMergeProps as (hooksProps: HooksProps, ownProps: OwnProps) => Props,
 ) =>
-  (Component: React.ComponentType<Props>): React.FunctionComponent<OwnProps> => {
+  (Component: React.ComponentType<Props>): ConnectedComponent<OwnProps, Props> => {
     type MemolizedProps = HooksProps & { $$ownProps: OwnProps }
     const Memolized = React.memo(({ $$ownProps, ...hooksProps }: MemolizedProps) => {
       const props = mergeProps(hooksProps as any as HooksProps, $$ownProps)
       return <Component {...props} />
     })
 
-    return (ownProps) => {
-      const hooksProps = mapHooks(ownProps) as React.PropsWithRef<MemolizedProps>
-      return <Memolized $$ownProps={ownProps} {...hooksProps} />
-    }
+    const Connected = (
+      (ownProps) => {
+        const hooksProps = mapHooks(ownProps) as React.PropsWithRef<MemolizedProps>
+        return <Memolized $$ownProps={ownProps} {...hooksProps} />
+      }
+    ) as ConnectedComponent<OwnProps, Props>
+
+    Connected.WrappedComponent = Component
+
+    return Connected
   }
