4b7705adc2adecd0fbab390fabb914f3de796452
iMasanari <14dd023@gmail.com>
2019-02-09 10:26:36 +0900

    refactor: Memo Component

--- src/react-hooks-connect.tsx
+++ src/react-hooks-connect.tsx
@@ -4,23 +4,43 @@ interface ConnectedComponent<OwnProps, Props> extends React.FunctionComponent<Ow
   WrappedComponent: React.ComponentType<Props>
 }
 
+type PrivateData<OwnProps, HooksProps, Props> = [
+  React.ComponentType<Props>,
+  OwnProps,
+  (hooksProps: HooksProps, ownProps: OwnProps) => Props
+]
+
+interface MemolizedProps<OwnProps, HooksProps, Props> {
+  REACT_HOOKS_CONNECT_PRIVATE_PROP: PrivateData<OwnProps, HooksProps, Props>
+}
+
+const Memolized = React.memo((
+  { REACT_HOOKS_CONNECT_PRIVATE_PROP: [Component, ownProps, mergeProps], ...hooksProps }: MemolizedProps<any, any, any>
+) => {
+  const props = mergeProps(hooksProps, ownProps)
+
+  return <Component {...props} />
+})
+
 const defaultMergeProps = (hooksProps: any, ownProps: any) => ({ ...ownProps, ...hooksProps })
 
-export default <OwnProps, HooksProps, Props = HooksProps>(
+const connect = <OwnProps, HooksProps, Props = HooksProps>(
   mapHooks: (ownProps: OwnProps) => HooksProps,
   mergeProps = defaultMergeProps as (hooksProps: HooksProps, ownProps: OwnProps) => Props,
 ) =>
   (Component: React.ComponentType<Props>): ConnectedComponent<OwnProps, Props> => {
-    type MemolizedProps = HooksProps & { $$ownProps: OwnProps }
-    const Memolized = React.memo(({ $$ownProps, ...hooksProps }: MemolizedProps) => {
-      const props = mergeProps(hooksProps as any as HooksProps, $$ownProps)
-      return <Component {...props} />
-    })
-
     const Connected = (
       (ownProps) => {
-        const hooksProps = mapHooks(ownProps) as React.PropsWithRef<MemolizedProps>
-        return <Memolized $$ownProps={ownProps} {...hooksProps} />
+        const _data: PrivateData<OwnProps, HooksProps, Props> = [Component, ownProps, mergeProps]
+        const privateData = React.useMemo(() => _data, _data)
+        const hooksProps = mapHooks(ownProps) as React.PropsWithRef<HooksProps>
+
+        return (
+          <Memolized
+            REACT_HOOKS_CONNECT_PRIVATE_PROP={privateData}
+            {...hooksProps}
+          />
+        )
       }
     ) as ConnectedComponent<OwnProps, Props>
 
@@ -28,3 +48,5 @@ export default <OwnProps, HooksProps, Props = HooksProps>(
 
     return Connected
   }
+
+export default connect
